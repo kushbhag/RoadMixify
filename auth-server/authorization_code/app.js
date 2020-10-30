9f71e831d3d52a4f93d8e986f240260bdb21a66e
Kush Bhagat <bhagat.kush.a@gmail.com>
2020-10-30 10:03:17 -0400

    '

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -14,7 +14,8 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
 var client_id = 'e1f23fa585bc4295898906c3e73498f4'; // Your client id
-var client_secret = '1431175d637045839a5b44b8ca2f9c62'; // Your secret
+var client_secret = ''; // Your secret
+
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
